d258bf6acf0dd84d1c87fbe6531e7d9f31a84724
C19H3R <karan1688.cse18@chitkara.edu.in>
2021-08-29 09:59:37 +0530

    rectangle stacks

---
+++ OOPS/Queue/HighestRectangle.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+#include<vector>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    arr.resize(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    vector<int> LeftArr;
+    LeftArr.resize(n);
+    vector<int> RightArr;
+    RightArr.resize(n);
+
+    stack<int> st;
+
+    //for leftsmall arr()
+
+    for (int i = n-1; i >=0 ; i--)
+    {
+        if(!st.empty()&&arr[st.top()]>=arr[i]){
+            st.pop();
+        }
+
+        if(st.empty())
+            LeftArr[i]=n;
+        else
+        {
+            LeftArr[i]=st.top();
+        }
+        st.push(i);
+    }
+    for(auto i : arr){
+        cout << i <<" ";
+    }
+     
+     cout << endl;
+    for(auto i : LeftArr){
+        cout << i <<" ";
+    }
+
+    return 0;
+}
